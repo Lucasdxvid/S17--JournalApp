0cf42f4a4901131e02075499bc9758f9a0116971
lucasdxvid <athl12311@gmail.com>
2023-05-03 18:48:41 -0300

    su

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,16 @@
-import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   //! Componentes que usan Material UI se envuelven en typography - Component cambia el elemento pero estilizado como MaterialUiy variant el h1 original
   return (
-    <>
-      <Typography variant={"h1"}>JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        deleniti quam, accusamus repudiandae dolorum expedita cumque dolore
+        nostrum id reiciendis incidunt dolorem placeat laudantium molestias
+        aperiam, aliquam vel in natus.
+      </Typography>
+    </JournalLayout>
   );
 };
