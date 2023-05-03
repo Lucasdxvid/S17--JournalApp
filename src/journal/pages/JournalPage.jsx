d0b20ceefd1efab2e0edece300da9357b59a9ca1
lucasdxvid <athl12311@gmail.com>
2023-05-03 17:11:17 -0300

    fix

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -5,8 +5,8 @@ export const JournalPage = () => {
   //! Componentes que usan Material UI se envuelven en typography - Component cambia el elemento pero estilizado como MaterialUiy variant el h1 original
   return (
     <>
-      <Typography variant={"h1"}>JournalPage</Typography>;
-      <MailOutline/>
+      <Typography variant={"h1"}>JournalPage</Typography>
+      <MailOutline />
     </>
   );
 };
