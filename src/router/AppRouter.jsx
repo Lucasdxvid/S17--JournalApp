c4999790593fe81298bf90427371ffd257595344
lucasdxvid <athl12311@gmail.com>
2023-05-29 12:23:24 -0300

    fix

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,27 +1,11 @@
-import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { FirebaseAuth } from "../firebase/config";
+import { useCheckAuth } from "../hooks";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { useDispatch, useSelector } from "react-redux";
 import { CheckingAuth } from "../ui/";
-import { onAuthStateChanged } from "firebase/auth";
-import { login, logout } from "../store/auth";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-
-  const dispatch = useDispatch();
-
-  //? Podemos estar pendientes a los cambios del usuario actual con firebase
-  useEffect(() => {
-    // 1er param Nuestro firebaseAuth, 2do funcion callback que ejecutaremos cuando se reciba el siguiente valor
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-      const { uid, email, displayName, photoURL } = user;
-      dispatch(login({ uid, email, displayName, photoURL }));
-    }); //Cuando el estado de la autentificacion cambia - Esto devuelve un OBSERVABLE (funcion que emite valores, cuando el estado de la funcion cambia esta funcion se vuelve a disparar)
-  }, []);
+  const status = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
